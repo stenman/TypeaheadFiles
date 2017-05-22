b025a78ec92054bfbb7264dcacd6ec431eeca25b
stenman <roger.stenman@gmail.com>
2017-05-22 16:06:43 +0200

    Added a  simple workflow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String args[]) {
+
+        // TODO: create a simple and lightweight swing(?) app with only one text box for searching (inc. search)
+        // TODO: make service out of program
+        // TODO: is it possible to start searching from a global command (windows)?
+        // TODO: create a construct to use for searching...
+        // TODO: get inc search working
+        // TODO: use a db or simply keep updated version in memory?
+        // TODO: look in configured directories recursively after files
+        // TODO: add options: file type(s) to search for
         System.out.println("test");
+
+
+
     }
 }
