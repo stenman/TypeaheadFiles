a74d393b2e662627be7c2dc5a32caad16df04064
stenman <roger.stenman@gmail.com>
2017-05-25 09:41:16 +0200

    Added logging

--- src/main/java/ServiceLauncher.java
+++ src/main/java/ServiceLauncher.java
@@ -1,8 +1,12 @@
 import org.apache.commons.daemon.Daemon;
 import org.apache.commons.daemon.DaemonContext;
 import org.apache.commons.daemon.DaemonInitException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServiceLauncher implements Daemon {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLauncher.class);
 
     public static void main(String args[]) {
         System.out.println("Hello, World!");
