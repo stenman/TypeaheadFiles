f83af173094dcf599af0fc9ccdd5fd254d201432
stenman <roger.stenman@gmail.com>
2017-05-27 10:58:17 +0200

    Install script and service now works properly

--- src/main/java/se/perfektum/typeaheadfiles/ServiceLauncher.java
+++ src/main/java/se/perfektum/typeaheadfiles/ServiceLauncher.java
@@ -9,25 +9,56 @@ import org.slf4j.LoggerFactory;
 public class ServiceLauncher implements Daemon {
 
     private static final Logger logger = LoggerFactory.getLogger(ServiceLauncher.class);
+    private static boolean stop = false;
 
     public static void main(String args[]) {
-        System.out.println("Hello, World!");
         logger.debug("Testing logger!");
+        System.out.println("Start GUI here if combo key was pressed!");
+    }
+    public static void startService() {
+        System.out.println("Starting service");
+        System.out.println("Waiting for combo key!");
+        while (!stop) {
+            try {
+                // TODO Implement combo key listener here!
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("running");
+        }
     }
 
+    public static void stopService() {
+        System.out.println("Stopping service");
+        stop = true;
+    }
+
+    public static void windowsService(String args[]) {
+        if ("start".equals(args[0])) {
+            startService();
+        } else if ("stop".equals(args[0])) {
+            stopService();
+        }
+    }
+
+    @Override
     public void init(DaemonContext daemonContext) throws DaemonInitException, Exception {
 
     }
 
+    @Override
     public void start() throws Exception {
 
     }
 
+    @Override
     public void stop() throws Exception {
 
     }
 
+    @Override
     public void destroy() {
 
     }
+
 }
